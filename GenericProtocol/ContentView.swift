2912985f2fac658c7b93c43bd4c30cc2f3c6ced7
your_username <your_email@gmail.com>
2022-06-21 14:11:56 +0200

    Initial Commit

---
+++ GenericProtocol/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GenericProtocol
+//
+//  Created by Dimitri POCHERON on 21/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenericProtocol/GenericProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericProtocolApp.swift
+//  GenericProtocol
+//
+//  Created by Dimitri POCHERON on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GenericProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
